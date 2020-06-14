9a6fc22931a7eeb8a69535dcb534ae4b498d5271
Yashvir-Rana <datasciyash@gmail.com>
2020-06-14 09:55:28 +0530

    import gf and replace the event loop

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,9 +1,9 @@
-import sys
 
 import pygame
 
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 def run_game():
     """initialize pygame, settings and screen object"""
@@ -21,11 +21,8 @@ def run_game():
 
     # start the main loop for the game
     while True:
+        gf.check_events()
         
-        # watch for keyboard and mouse event
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
 
         # redraw the screen during each pass through the loop
         screen.fill(ai_settings.bg_color)
