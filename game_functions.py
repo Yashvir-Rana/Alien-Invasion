883af8bcee3ac5bc3cf891c22680f2bddef9bbf8
Yashvir-Rana <datasciyash@gmail.com>
2020-06-16 21:18:09 +0530

    responding to akeypress

--- game_functions.py
+++ game_functions.py
@@ -2,11 +2,16 @@ import sys
 
 import pygame
 
-def check_events():
+def check_events(ship):
     """respond to keypress and mouse events"""
     for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+        elif event.type == pygame.KEYDOWN:
+            if event.type == pygame.K_RIGHT:
+                #move the ship to the right
+                ship.rect.centerx += 1
 
 def update_screen(ai_settings, screen, ship):
     """update images on the screen and flip to the new screen"""
