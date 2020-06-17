5052d95c80b3097bd43af60a69ce5ae1c4c151eb
Yashvir-Rana <datasciyash@gmail.com>
2020-06-17 12:23:32 +0530

    ship speed factor

--- settings.py
+++ settings.py
@@ -3,7 +3,9 @@ class Settings():
 
     def __init__(self):
         """initialize the game settings"""
-        #scrceen settings
+        #screen settings
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
+        #ship setting
+        self.ship_speed_factor = 1.5
